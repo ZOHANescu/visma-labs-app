255d7700d02442bd5b7f0df4bebd9d8f33f397aa
Stefan <stefanhera@gmail.com>
2022-06-02 14:50:56 +0300

    Added limit of displayed employees in the table.

--- src/main/client/src/Pages/EmployeeList/useEmployeeList.ts
+++ src/main/client/src/Pages/EmployeeList/useEmployeeList.ts
@@ -4,18 +4,36 @@ import {Employee} from "../EmployeeForm/EmployeeForm.types";
 
 export function useEmployeeList(): EmployeeListProps{
 
+
+    const [pageNumber, setPageNumber] = useState<number>(0);
+    const [pageSize, setPageSize] = useState<number>(10);
+
     const [employees, setEmployees] = useState<Employee[]>([]);
 
     async function fetchData(){
-        const newEmployees = await window.fetch("/api/employee/", {
+
+        const params = new URLSearchParams();
+        params.set("pageNumber", "" + pageNumber);
+        params.set("pageSize", "" + pageSize);
+
+        /*const newEmployees = await window.fetch("/api/employee/", {
             method: "GET",
             headers: {
                 'Content-Type': 'application/json'
             }
         }).then(r => r.json())
-            .then(json => json.content);
+            .then(json => json.content);*/
 
-        setEmployees(newEmployees);
+        const response = await window.fetch("/api/employee/?" + params.toString(), {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        const json = await response.json();
+        const newValues = json.content;
+
+        setEmployees(newValues);
     }
 
     useEffect(() => {
